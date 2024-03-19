33f3c70954b325f72ca20d3c359dce380f903aec
youness mojahid <mojahidyouness0@gmail.com>
2024-03-19 10:30:22 -0700

    lope

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ if number < 0:
     sign = -1
 else:
     sign = 1
-number_ *= sign 
+number_ *= sign
 if number_ > 5:
     out += '{} '.format(number_) + 'and is greater than 5'
-elif  number_ == 0:
+elif number_ == 0:
     out += '{} '.format(number_) + 'and is 0'
 else:
     out += '{} '.format(number_) + 'and is less than 6 and not 0'
